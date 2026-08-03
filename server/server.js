1bfe0ea2015d5b5b2ffc597c2b4391b1fc02ffec
shilpithaM <shilpitha6@gmail.com>
2026-08-03 15:10:08 +0530

    Database setup

--- server/server.js
+++ server/server.js
@@ -2,10 +2,18 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const mongoose = require("mongoose");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+  
 
 app.get("/", (req, res) => {
   res.send("🚀 Campus Innovation Hub Backend is Running...");
